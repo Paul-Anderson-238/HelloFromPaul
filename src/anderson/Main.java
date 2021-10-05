c9a2cae5bc8ae3603120888978054c151fa370e7
Rex-1nvictus <and13005@byui.edu>
2021-10-05 16:42:42 -0600

    remove unnecessary comments

--- src/anderson/Main.java
+++ src/anderson/Main.java
@@ -3,7 +3,6 @@ package anderson;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
         System.out.println("Hello, my name is <your name>");
     }
 }
