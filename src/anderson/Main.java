942bb18609c56a7bba1a14dbeb721e43b210e26b
smithbh1 <smithbh129@gmail.com>
2021-10-14 18:21:28 -0600

    team member update

--- src/anderson/Main.java
+++ src/anderson/Main.java
@@ -2,7 +2,9 @@ package anderson;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello, my name is Paul");
+    public static void main(String[] args)
+    {
+        System.out.print("Hello, my name is Paul");
+        System.out.println("Hello Paul, my name is Brian");
     }
 }
